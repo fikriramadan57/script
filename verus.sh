b38490174da214168d5d8361ce19aa11874b99a8
fikriramadan57 <37709452+fikriramadan57@users.noreply.github.com>
2021-07-12 01:24:21 +0700

    update verus.sh

---
+++ verus.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+hellminer.exe -c stratum+tcp://na.luckpool.net:3956#xnsub -u 18LU2dKMj1JWjpPsRqFE8tyiPU2jnfuzVA.fikri -p x --cpu 4
